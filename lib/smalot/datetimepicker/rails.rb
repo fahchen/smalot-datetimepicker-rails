0dfef83a9c9d3af94359a493e654b99416204fb5
fahchen <dev.fah@gmail.com>
2014-07-26 15:15:25 +0800

    init

---
+++ lib/smalot/datetimepicker/rails.rb
@@ -0,0 +1,9 @@
+require "smalot/datetimepicker/rails/version"
+
+module Smalot
+  module Datetimepicker
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/smalot/datetimepicker/rails/version.rb
@@ -0,0 +1,7 @@
+module Smalot
+  module Datetimepicker
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
